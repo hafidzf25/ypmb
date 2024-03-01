56a5d43b14fb4336a9abc8640770cda1cc61a367
bedulsundae <hafidzfleur@gmail.com>
2024-03-01 14:26:51 +0700

    Href pelatihan page

--- resources/views/layouts/headfooter.blade.php
+++ resources/views/layouts/headfooter.blade.php
@@ -20,14 +20,14 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-custom">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#" id="cerdas">Cerdas</a>
+          <a class="navbar-brand" href="{{ url('/') }}" id="cerdas">Cerdas</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active me-1 ms-2" aria-current="page" href="#">Pelatihan</a>
+                <a class="nav-link active me-1 ms-2" aria-current="page" href="{{ url('pelatihan') }}">Pelatihan</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle me-2 ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
---
+++ resources/views/pelatihan.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.headfooter')
+
+@section('content')
+    
+@endsection
