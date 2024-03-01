42e041eea46c05a86222d974c1861d5d4e2201c3
bedulsundae <hafidzfleur@gmail.com>
2024-03-01 14:57:18 +0700

    pelatihan update

--- resources/views/pelatihan.blade.php
+++ resources/views/pelatihan.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid judul-radius">
-        <p style="font-size: 4.4em;">Temukan Pelatihan Untuk Tingkatkan Kompetensi Anda</p>
-        <input style="outline: 0.001em solid black;" class="form-control me-4 custom-rounded search-custom-width" type="search" placeholder="Cari Pelatihan" aria-label="Search">
+        <p style="font-size: 4.2em;">Temukan Pelatihan Untuk Tingkatkan Kompetensi Anda</p>
+        <input style="outline: 2px solid #666666; width: 30em; height:3em; margin-top:50px; font-size:1.2em;" class="form-control me-4 custom-rounded search-custom-width" type="search" placeholder="Cari Pelatihan" aria-label="Search">
     </div>
 @endsection
