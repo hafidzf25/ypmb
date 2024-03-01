b0eb0c1ad2c28733cafaf597f02daa352a2899f3
bedulsundae <hafidzfleur@gmail.com>
2024-03-01 13:57:54 +0700

    Laravel template + Homepage

---
+++ resources/views/layouts/headfooter.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/Logo/ypmb.png') }}" type="image/icon type">
+    <title>Home page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><!-- Load Font Awesome CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#" id="cerdas">Cerdas</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active me-1 ms-2" aria-current="page" href="#">Pelatihan</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle me-2 ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Jenjang
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">SD</a></li>
+                  <li><a class="dropdown-item" href="#">SMP</a></li>
+                  <li><a class="dropdown-item" href="#">SMA</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-4 custom-rounded search-custom-width" type="search" placeholder="Cari Pelatihan" aria-label="Search">
+              <button class="btn btn-info text-white custom-rounded custom-width" type="submit">Masuk</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <br>
+
+      @yield('content')
+      
+      <div class="container-fluid" style="padding: 3em; background-color: #38B6FF; color: white;">
+        <div class="row align-items-center">
+          <div class="col-4">
+            <span id="cerdas">Cerdas</span>
+          </div>
+          <div class="col-4 text-center">
+            <p>
+              <img src="{{ asset('images/footer-1.png') }}" alt="" style="width: 30em;">
+            </p>
+            <span>
+              <a href="https://twitter.com">
+                <img src="{{ asset('images/socmed-x.png') }}" alt="" style="padding: 0.5em;">
+              </a>
+              <a href="https://instagram.com">
+                <img src="{{ asset('images/socmed-ig.png') }}" alt="" style="padding: 0.5em;">
+              </a>
+              <a href="https://youtube.com">
+                <img src="{{ asset('images/socmed-yt.png') }}" alt="" style="padding: 0.5em;">
+              </a>
+              <a href="https://facebook.com">
+                <img src="{{ asset('images/socmed-fb.png') }}" alt="" style="padding: 0.5em;">
+              </a>
+            </span>
+          </div>
+          <div class="col-4 text-end">
+            <img src="{{ asset('images/telephone.png') }}" alt="">
+            <span>Kontak</span>
+            <span>(021) 888888</span>
+          </div>
+        </div>
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.headfooter')
+
+@section('content')
+    <div class="container-fluid" style="padding-left: 3em; padding-right: 3em; padding-bottom: 1em;">
+        <div class="row justify-content-center">
+            <div class="col carousel-container">
+              <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="2.png" class="d-block w-100 carousel-img-radius" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="3.png" class="d-block w-100 carousel-img-radius" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="2.png" class="d-block w-100 carousel-img-radius" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                  <span class="" aria-hidden="true"></span>
+                    <img src="prev_page.png" alt="Next" style="width: 50px; height: auto;">
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                  <span class="" aria-hidden="true">
+                    <img src="next_page.png" alt="Next" style="width: 50px; height: auto;">
+                  </span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="container-fluid" style="padding-top: 3em;">
+        <div class="row text-center align-items-center">
+          <div class="col" style="font-weight: bolder; font-size: 2em;">
+            Tentang Cerdas
+          </div>
+        </div>
+      </div>
+      
+      <div class="container-fluid" style="padding: 35px;">
+        <div class="row">
+          <div class="col-6" style="font-weight: bolder; font-size: 28px;">
+            <img src="tentangcerdas.png" alt="" style="max-width: 100%; height: auto;">
+          </div>
+          <div class="col-6" style="font-size: 18px;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Eu nisl nunc mi ipsum. Dignissim sodales ut eu sem integer vitae justo eget magna. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Suspendisse interdum consectetur libero id faucibus nisl. Commodo elit at imperdiet dui. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Aliquet nibh praesent tristique magna sit amet. Porttitor lacus luctus accumsan tortor. Mauris pharetra et ultrices neque ornare aenean euismod. Gravida quis blandit turpis cursus in hac. Ut pharetra sit amet aliquam. 
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid" style="padding-left: 2em; padding-right: 2em;">
+        <div class="row align-items-center">
+          <div class="col" style="font-weight: bolder; font-size: 2em;">
+            Pelatihan Populer
+          </div>
+          <div class="col text-end" style="font-weight: bolder; font-size: 1.2em;">
+            <a href="" style="text-decoration: none; color: inherit;">
+              Lihat Semua
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row row-cols-1 row-cols-md-4 g-4" style="padding: 2em;">
+        <div class="col">
+          <div class="card">
+            <img src="tree2.png" class="card-img-top" alt="..." style="height: 10em; ">
+            <div class="card-body">
+              <h5 class="card-title" style="font-weight: bolder;">Pelatihan Deteksi Dini: Analisa...</h5>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em; margin-top: 1.5em;">
+                <img src="kalender.png" alt=""> 16 Feb 2024 - 19 Feb 2024
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em;">
+                <img src="people.png" alt=""> Peserta 4077
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 1.8em;">
+                <img src="star.png" alt=""> 4.8
+              </p>
+              <div style="text-align: center;">
+                <button class="btn btn-info text-white custom-rounded card-width" type="submit">Lihat Selengkapnya</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="tree1.png" class="card-img-top" alt="..." style="height: 10em; ">
+            <div class="card-body">
+              <h5 class="card-title" style="font-weight: bolder;">Pelatihan Deteksi Dini: Analisa...</h5>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em; margin-top: 1.5em;">
+                <img src="kalender.png" alt=""> 16 Feb 2024 - 19 Feb 2024
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em;">
+                <img src="people.png" alt=""> Peserta 4077
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 1.8em;">
+                <img src="star.png" alt=""> 4.8
+              </p>
+              <div style="text-align: center;">
+                <button class="btn btn-info text-white custom-rounded card-width" type="submit">Lihat Selengkapnya</button>
+              </div>  
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="tree2.png" class="card-img-top" alt="..." style="height: 10em; ">
+            <div class="card-body">
+              <h5 class="card-title" style="font-weight: bolder;">Pelatihan Deteksi Dini: Analisa...</h5>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em; margin-top: 1.5em;">
+                <img src="kalender.png" alt=""> 16 Feb 2024 - 19 Feb 2024
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em;">
+                <img src="people.png" alt=""> Peserta 4077
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 1.8em;">
+                <img src="star.png" alt=""> 4.8
+              </p>
+              <div style="text-align: center;">
+                <button class="btn btn-info text-white custom-rounded card-width" type="submit">Lihat Selengkapnya</button>
+              </div>  
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="tree1.png" class="card-img-top" alt="..." style="height: 10em; ">
+            <div class="card-body">
+              <h5 class="card-title" style="font-weight: bolder;">Pelatihan Deteksi Dini: Analisa...</h5>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em; margin-top: 1.5em;">
+                <img src="kalender.png" alt=""> 16 Feb 2024 - 19 Feb 2024
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 0.6em;">
+                <img src="people.png" alt=""> Peserta 4077
+              </p>
+              <p class="card-text align-items-center" style="margin-bottom: 1.8em;">
+                <img src="star.png" alt=""> 4.8
+              </p>
+              <div style="text-align: center;">
+                <button class="btn btn-info text-white custom-rounded card-width" type="submit">Lihat Selengkapnya</button>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
