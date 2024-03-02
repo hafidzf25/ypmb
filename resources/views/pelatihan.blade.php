5540f51ccf9ed1987cb0c8f196d90ffe80322b70
Legi <legikuswandi@gmail.com>
2024-03-02 14:36:14 +0700

    aaaa

--- resources/views/pelatihan.blade.php
+++ resources/views/pelatihan.blade.php
@@ -5,7 +5,6 @@
         <p style="font-size: 4.2em;">Temukan Pelatihan Untuk Tingkatkan Kompetensi Anda</p>
         <input style="outline: 2px solid #666666; width: 30em; height:2.5em; margin-top:50px; font-size:1.2em; padding-left:1.2em" class="form-control me-4 custom-rounded search-custom-width" type="search" placeholder="Cari Pelatihan" aria-label="Search">
     </div>
-
     <div class="container-fluid" style="padding: 3.5em;">
         <h1 style="font-weight: 900;">Pelatihan Terbaru</h1>
         <select class="form-select" aria-label="Default select example" style="width: 15em;">
