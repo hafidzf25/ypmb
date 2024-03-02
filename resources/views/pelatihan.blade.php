6c515eb3dd57eebbbee1929a45d079424988903b
Legi <legikuswandi@gmail.com>
2024-03-02 17:10:17 +0700

    search

--- resources/views/pelatihan.blade.php
+++ resources/views/pelatihan.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
     <div class="container-fluid judul-radius">
-        <p style="font-size: 4.2em;">Temukan Pelatihan Untuk Tingkatkan Kompetensi Anda</p>
-        <input style="outline: 2px solid #666666; width: 30em; height:2.5em; margin-top:50px; font-size:1.2em; padding-left:1.2em" class="form-control me-4 custom-rounded search-custom-width" type="search" placeholder="Cari Pelatihan" aria-label="Search">
+        <div class="row align-items-center">
+            <div class="col">
+                <p style="font-size: 4.2em;">Temukan Pelatihan Untuk Tingkatkan Kompetensi Anda</p>
+            </div>
+            <div class="col-auto">
+                <form action="{{ route('pelatihan') }}" method="GET" class="input-group" style="margin-top: 50px;">
+                    <input type="text" name="search" class="form-control me-4 custom-rounded search-custom-width" placeholder="Cari Pelatihan" aria-label="Search">
+                    <button type="submit" class="btn custom-rounded" style="background-color: #FEAD01">Cari</button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="container-fluid" style="padding: 3.5em;">
         <h1 style="font-weight: 900;">Pelatihan Terbaru</h1>
@@ -21,7 +30,7 @@
                     <div class="card">
                         <img src="{{ asset('images/cards-2.png') }}" class="card-img-top" alt="..." style="height: 10em; ">
                         <div class="card-body">
-                            <h5 class="card-title" style="font-weight: bolder;">{{ Illuminate\Support\Str::limit($pelatihan->nama_pelatihan, 39) }}</h5>
+                            <h5 class="card-title" style="font-weight: bolder;">{{ Illuminate\Support\Str::limit($pelatihan->nama_pelatihan, 30) }}</h5>
                         <p class="card-text align-items-center" style="margin-bottom: 0.6em; margin-top: 1.5em;">
                             <i class="bi bi-calendar-week-fill" style="color: #38B6FF"></i>
                             <img src="kalender.png" alt=""> {{ $pelatihan->tanggal_awal }} - {{ $pelatihan->tanggal_akhir }}
