37af90918bccb145c7c33c10e1f4c9f8bba6cd0a
Legi <legikuswandi@gmail.com>
2024-06-10 17:48:49 +0700

    add some bug

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -43,7 +43,7 @@ class RegisterController extends Controller
 
         $details = [
             'name' => $request->name,
-            'website' => 'www.cerdas.com',
+            'website' => 'www.YPMB-Berbagi.com',
             'datetime' => date('Y-m-d H:i:s'),
             'url' => request()->getHttpHost().'/register/verify/'.$str
         ];
