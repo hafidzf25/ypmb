5b0b129955b0d54b88868a4bf1f7bee6784d20a4
IqbalFadhilah <iqbalfadhilah684@gmail.com>
2024-06-14 07:21:33 +0700

    update dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Seminar;
 use App\Models\Pelatihan;
+use App\Models\Admin;
 
 class HomeController extends Controller
 {
@@ -15,7 +16,18 @@ class HomeController extends Controller
         $totalUser = User::count();
         $totalSeminar = Seminar::count();
         $totalPelatihan = Pelatihan::count();
+        $totalPengurus = Admin::count();
 
-        return view('admin.dashboard', ['totalUser' => $totalUser, 'totalSeminar' => $totalSeminar, 'totalPelatihan' => $totalPelatihan]);
+        $seminar_terbaru = Seminar::select('id_seminar', 'nama_seminar', 'tanggal_seminar', 'waktu_seminar','status')
+                                        ->orderBy('id_seminar', 'desc')
+                                        ->take(5)
+                                        ->get();
+
+        $pelatihan_terbaru = Pelatihan::select('id_pelatihan', 'nama_pelatihan', 'tanggal_awal', 'tanggal_akhir','status')
+                                        ->orderBy('id_pelatihan', 'desc')
+                                        ->take(5)
+                                        ->get();
+
+        return view('admin.dashboard', ['totalUser' => $totalUser, 'totalSeminar' => $totalSeminar, 'totalPelatihan' => $totalPelatihan, 'totalPengurus' => $totalPengurus, 'seminar_terbaru' => $seminar_terbaru, 'pelatihan_terbaru' => $pelatihan_terbaru]);
     }
 }
