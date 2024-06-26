b0ce05879bc72b1156f233117af7bf5f5aa6c10b
IqbalFadhilah <iqbalfadhilah684@gmail.com>
2024-06-26 22:19:42 +0700

    delete "forgot pass" in login admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -64,9 +64,6 @@
           <!-- /.col -->
         </div>
       </form>
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
     </div>
     <!-- /.card-body -->
   </div>
